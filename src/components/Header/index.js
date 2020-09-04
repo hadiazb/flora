df802ae9087e4d5498d11cc1a1519033165205e1
hadiazb <hadiazb@unal.edu.co>
2020-09-04 18:10:50 -0500

    [START] Project

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+import Nav from '../Nav/index';
+
+const Header = () => {
+  return(
+    <header className='header container'>
+      <Nav/>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './index.scss';
+import Logo from '../../assets/logo.png';
+
+
+const Nav = () => {
+  return (
+    <nav className='nav'>
+      <img className='nav_img' src={Logo} alt="Logo floristeria"/>
+      <div className='nav_div'>
+        <a className='language' href="#">ENG</a>
+        <ul className='nav_ul'>
+          <li>
+            <a className='anchor' href="#">
+              QUIENES SOMOS
+            </a>
+          </li>
+          <li>
+            <a className='anchor strong nav_ul-blue' href="#">
+              VARIEDADES
+            </a>
+          </li>
+          <li>
+            <a className='anchor strong nav_ul-green' href="#">
+              INSUMOS
+            </a>
+          </li>
+          <li>
+            <a className='anchor' href="#">
+              CONTACTO
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
